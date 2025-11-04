fc752e562815f6d3d23a660c1390a8e1e09ab1a1
lorasvlr <lora.lorinet@gmail.com>
2025-11-04 14:43:12 +0100

    headerback backoffice dossier

--- views/backoffice/partials/headerBack.php
+++ views/backoffice/partials/headerBack.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../public/style/styleTemplate.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <title>header Back</title>
 </head>
 <body>
@@ -10,7 +12,7 @@
         <div class="headerMain">
         <div class="logoNom">
 
-            <a href="AccueilVendeur"><img src="../../public/images/LogoAlizonBack.svg" alt="Alizon"></a>
+            <a href="AccueilVendeur"><img src="../../../public/images/LogoAlizonBack.svg" alt="Alizon"></a>
 
         </div>
     </header>
