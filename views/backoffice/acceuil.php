d422f38c5b08db28a486977c85c384f4e46e6550
marechalg <guewenmarechal@protonmail.com>
2025-11-04 16:32:24 +0100

    commit

---
+++ views/backoffice/acceuil.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Alizon - Backoffice</title>
+
+        <link rel="stylesheet" href="../../public/style.css">
+    </head>
+
+    <body>
+        <header>
+            <div>
+                <img src="../../public/images/backoffice/panier.svg" alt="icon">
+                <h1>Alizon</h1>
+            </div>
+            
+            <nav>
+                <figure>
+                    <figcaption>Nouveau Produit</figcaption>
+                    <img src="../../public/images/backoffice/panierPlusLight.svg">
+                </figure>
+                <button onclick=""></button>
+                <button onclick=""></button>
+                <button onclick=""></button> 
+            </nav>
+        </header>
+    </body>
+</html>
