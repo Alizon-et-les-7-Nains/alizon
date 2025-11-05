d564dc58cd6cc10e27b57a7e46df1c94fea8e891
marechalg <guewenmarechal@protonmail.com>
2025-11-05 11:35:11 +0100

    setup acceuilBackoffice

---
+++ views/backoffice/acceuil.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Alizon</title>
+
+        <link rel="stylesheet" href="../../public/style.css">
+    </head>
+
+    <body>
+        <?php require_once './partials/headerMain.php' ?>
+
+        <main class="acceuilBackoffice">
+            
+        </main>
+    </body>
+</html>
