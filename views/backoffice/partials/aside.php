4d6a4a0ee69a38ad9df8cc67e2c19ad207244f95
marechalg <guewenmarechal@protonmail.com>
2025-11-05 22:49:41 +0100

    indentation

--- views/backoffice/partials/aside.php
+++ views/backoffice/partials/aside.php
@@ -1,65 +1,48 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+<aside class="backoffice">
+    <ul>
+        <li class="aside-btn here">
+            <figure>
+                <img src="/public/images/homeDarkBlue.svg">
+                <figcaption>Accueil</figcaption>
+            </figure>
+        </li>
+        <li class="aside-btn">
+            <figure>
+                <img src="/public/images/courbeDark.svg">
+                <figcaption>Statistiques</figcaption>
+            </figure>
+        </li>
+        <li class="aside-btn">
+            <figure>
+                <img src="/public/images/cartDarkBlue.svg">
+                <figcaption>Produits</figcaption>
+            </figure>
+        </li>
+        <li class="aside-btn">
+            <figure>
+                <img src="/public/images/boiteDark.svg">
+                <figcaption>Stock</figcaption>
+            </figure>
+        </li>
+        <li class="aside-btn">
+            <figure>
+                <img src="/public/images/chatDark.svg">
+                <figcaption>Avis</figcaption>
+            </figure>
+        </li>
+        <li class="aside-btn">
+            <figure>
+                <img src="/public/images/cartCheckDark.svg">
+                <figcaption>Commandes</figcaption>
+            </figure>
+        </li>
+        <li class="aside-btn">
+            <figure>
+                <img src="/public/images/enveloppeDark.svg">
+                <figcaption>Retours</figcaption>
+            </figure>
+        </li>
+    </ul>
 
-        <link rel="stylesheet" href="/public/style.css">
-    </head>
-
-    <body class="backoffice">
-        <?php require_once './headerMain.php' ?>
-
-        <aside class="backoffice">
-            <ul>
-                <li class="aside-btn here">
-                    <figure>
-                        <img src="/public/images/homeDarkBlue.svg">
-                        <figcaption>Accueil</figcaption>
-                    </figure>
-                </li>
-                <li class="aside-btn">
-                    <figure>
-                        <img src="/public/images/courbeDark.svg">
-                        <figcaption>Statistiques</figcaption>
-                    </figure>
-                </li>
-                <li class="aside-btn">
-                    <figure>
-                        <img src="/public/images/cartDarkBlue.svg">
-                        <figcaption>Produits</figcaption>
-                    </figure>
-                </li>
-                <li class="aside-btn">
-                    <figure>
-                        <img src="/public/images/boiteDark.svg">
-                        <figcaption>Stock</figcaption>
-                    </figure>
-                </li>
-                <li class="aside-btn">
-                    <figure>
-                        <img src="/public/images/chatDark.svg">
-                        <figcaption>Avis</figcaption>
-                    </figure>
-                </li>
-                <li class="aside-btn">
-                    <figure>
-                        <img src="/public/images/cartCheckDark.svg">
-                        <figcaption>Commandes</figcaption>
-                    </figure>
-                </li>
-                <li class="aside-btn">
-                    <figure>
-                        <img src="/public/images/enveloppeDark.svg">
-                        <figcaption>Retours</figcaption>
-                    </figure>
-                </li>
-            </ul>
-        </aside>
-
-        <?php require_once './footer.php' ?>
-
-        <script type="module" src="/public/script.js"></script>
-    </body>
-</html>
+    <script type="module" src="/public/script.js"></script>
+</aside>
