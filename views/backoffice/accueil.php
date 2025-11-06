5baa3920e96b55fa499ecb30a398267f6476721d
marechalg <guewenmarechal@protonmail.com>
2025-11-06 10:45:29 +0100

    bon debut

--- views/backoffice/accueil.php
+++ views/backoffice/accueil.php
@@ -14,7 +14,63 @@
         <?php require_once './partials/aside.php' ?>
 
         <main class="acceuilBackoffice">
-            
+            <section>
+                <h1>Derniers Bilans</h1>
+                <article>
+                    <table>
+                        <thead>
+                            <tr>
+                                <td><button class="here">Journalier</button></td>
+                                <td><button>Hebdomadaire</button></td>
+                                <td><button>Mensuel</button></td>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            <tr>
+                                <td>Nombre de ventes</td>
+                                <td colspan=2>Chiffre d'affaires</td>
+                            </tr>
+                            
+                            <tr>
+                                <td>
+                                    <figure>
+                                        <img src="/public/images/arrowDestonks.svg">
+                                        <figcaption class="neg">46</figcaption>
+                                    </figure>
+                                </td>
+                                <td>
+                                    <figure colspan=2>
+                                        <img src="/public/images/arrowStonks.svg">
+                                        <figcaption class="pos">1.634,50€</figcaption>
+                                    </figure>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </article>
+                <a href="" title="Voir plus"><img src="/public/images/infoDark.svg"></a>
+            </section>
+
+            <section>
+                <h1>Stocks Faible</h1>
+                <a href="" title="Voir plus"><img src="/public/images/infoDark.svg"></a>
+            </section>
+
+            <section>
+                <h1>Dernières Commandes</h1>
+                <a href="" title="Voir plus"><img src="/public/images/infoDark.svg"></a>
+            </section>
+
+            <section>
+                <h1>Derniers Avis</h1>
+                <a href="" title="Voir plus"><img src="/public/images/infoDark.svg"></a>
+            </section>
+
+            <section>
+                <h1>Produits en Vente</h1>
+                <a href="" title="Voir plus"><img src="/public/images/infoDark.svg"></a>
+            </section>
         </main>
 
         <?php require_once './partials/footer.php' ?>
