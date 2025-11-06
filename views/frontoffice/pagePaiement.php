70193cc132cdcb8ed9b3f9fbad09b7ab1f114d24
Timéo <timeodoom@gmail.com>
2025-11-06 10:52:56 +0100

    Add payment page styles and structure, including responsive design adjustments

--- views/frontoffice/pagePaiement.php
+++ views/frontoffice/pagePaiement.php
@@ -8,11 +8,59 @@
     <title>Paiement de votre panier</title>
 </head>
 
-<body>
-    <?php include_once 'partials/header.php'; ?>
-    <main class="pagePaiement">
-        <h1>Paiement de votre panier</h1>
+<body class="pagePaiement">
+    <?php include '../../views/frontoffice/partials/headerConnecte.php'; ?>
+
+    <main>
+
+        <div class="parent">
+            <div class="infosLivraison">
+                <h3>1 - Informations pour la livraison : </h3>
+
+                <form class="form-livraison" autocomplete="on" novalidate>
+                    <label class="sr-only" for="adresse">Adresse de livraison</label>
+                    <input id="adresse" name="adresse" type="text" placeholder="Adresse de livraison"
+                        class="input input--large">
+
+                    <div class="row">
+                        <label class="sr-only" for="codepostal">Code postal</label>
+                        <input id="codepostal" name="codepostal" type="text" placeholder="Code postal"
+                            class="input input--half">
+
+                        <label class="sr-only" for="ville">Ville</label>
+                        <input id="ville" name="ville" type="text" placeholder="Ville" class="input input--half">
+                    </div>
+
+                    <label class="checkbox">
+                        <input type="checkbox" id="facturation" name="facturation">
+                        <span>Adresse de facturation différente</span>
+                    </label>
+                </form>
+            </div>
+            <div class="recapPanier">
+
+            </div>
+            <div class="paiement">
+                <h3>2 - Informations de paiement : </h3>
+                <input type="text">
+                <input type="text">
+
+                <input type="text">
+                <input type="text">
+            </div>
+            <div class="conditionGen">
+                <h3>5 - Accepter les conditions génrales
+                    et mentions légales</h3>
+
+                <input type="checkbox" name="" id="">
+            </div>
+        </div>
+
+
     </main>
-    <?php include_once 'partials/footer.php'; ?>
+
+    <?php include '../../views/frontoffice/partials/footerConnecte.php'; ?>
+
+</body>
 
 </html>
