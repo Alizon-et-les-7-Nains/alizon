73e57f33d2e413bb311c23c4097f7ad94f2f0490
Timéo <timeodoom@gmail.com>
2025-11-06 09:17:57 +0100

    struct

--- views/frontoffice/pagePaiement.php
+++ views/frontoffice/pagePaiement.php
@@ -1,19 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../public/style.css">
-    <title>Page de connexion</title>
+    <title>Paiement de votre panier</title>
 </head>
+
 <body>
-    <?php include '../../views/frontoffice/partials/headerConnecte.php'; ?>
-
-    <main>
-
-
+    <?php include_once 'partials/header.php'; ?>
+    <main class="pagePaiement">
+        <h1>Paiement de votre panier</h1>
     </main>
+    <?php include_once 'partials/footer.php'; ?>
 
-    <?php include '../../views/frontoffice/partials/footerConnecte.php'; ?>
-</body>
 </html>
