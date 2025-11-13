29ae5179e8c6ac2888983ce4b76cc83d66ebadbc
marechalg <guewenmarechal@protonmail.com>
2025-11-13 21:46:18 +0100

    Refactor table structure and styles in backoffice views
    - Updated the HTML structure of the table in accueil.php to remove the border attribute for better styling control.
    - Enhanced the SCSS for avis articles to improve layout and styling, including:
      - Added flexbox layout for better alignment and spacing.
      - Defined styles for table elements, including borders, padding, and background colors.
      - Improved image styling within figures for better presentation.
      - Adjusted font sizes and weights for better readability.

--- views/backoffice/accueil.php
+++ views/backoffice/accueil.php
@@ -105,7 +105,7 @@ require_once '../../controllers/date.php';
     foreach ($avis as $avi) {
         $imagesAvis = ($pdo->query(str_replace('$idClient', $avi['idClient'], str_replace('$idProduit', $avi['idProduit'], file_get_contents('../../queries/imagesAvis.sql')))))->fetchAll(PDO::FETCH_ASSOC);
         $html = "
-        <table border=2>
+        <table>
             <tr>
                 <td rowspan=2>
                     <figure>
