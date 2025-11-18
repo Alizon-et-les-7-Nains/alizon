6d2649cd08f216bb83ff5e84238041f28ddb350b
lorasvlr <lora.lorinet@gmail.com>
2025-11-18 16:58:53 +0100

    BDD Compte vendeur

---
+++ controllers/creerCompteVendeur.php
@@ -0,0 +1,37 @@
+<?php
+    require_once "pdo.php";
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $pseudo = $_POST['pseudo'] ?? '';
+    $prenom = $_POST['prenom'] ?? '';
+    $nom = $_POST['nom'] ?? '';
+    $email = $_POST['email'] ?? '';
+    $num_tel = $_POST['num_tel'] ?? '';
+    $mdp = $_POST['mdp'] ?? '';
+    $date_naissance = $_POST['birthdate'] ?? '';
+
+    $sql = "INSERT INTO _client 
+        (dateNaissance, prenom, nom, email, mdp, noTelephone, pseudo)
+        VALUES (:dateNaissance, :prenom, :nom, :email, :mdp, :noTelephone, :pseudo)";
+
+    $stmt = $pdo->prepare($sql);
+
+    $stmt->execute([
+        ':dateNaissance' => $date_naissance,
+        ':prenom' => $prenom,
+        ':nom' => $nom,
+        ':email' => $email,
+        ':mdp' => $mdp,
+        ':noTelephone' => $num_tel,
+        ':pseudo' => $pseudo,
+    ]);
+    }
+
+
+    session_start();  
+    $id_session = session_id();
+    $_SESSION['id_session'] = $id_session;
+    header('Location: ../views/backoffice/accueil.php');
+    exit();
+?>
