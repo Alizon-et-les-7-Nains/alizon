1e639f4fa46c18ea752180a56f72468fc8aeaeae
lorasvlr <lora.lorinet@gmail.com>
2025-11-18 17:26:57 +0100

    BDD vendeur v2

--- controllers/creerCompteVendeur.php
+++ controllers/creerCompteVendeur.php
@@ -3,36 +3,34 @@
 
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    $nom_contact = $_POST['nom_contact'] ?? '';
-    $prenom_contact = $_POST['prenom_contact'] ?? '';
+    $nom_contact = $_POST['nom'] ?? '';
+    $prenom = $_POST['prenom_contact'] ?? '';
     $email = $_POST['email'] ?? '';
-    $num_tel = $_POST['num_tel'] ?? '';
-    $nom_utilisateur = $_POST['nom_utilisateur'] ?? '';
+    $noTelephone = $_POST['num_tel'] ?? '';
+    $pseudo = $_POST['nom_utilisateur'] ?? '';
     $mdp = $_POST['mdp'] ?? '';
-    $date_naissance = $_POST['date_naissance'] ?? '';
-    $confimer_mdp = $_POST['confimer_mdp'] ?? ''; 
-    $num_siren = $_POST['num_siren'] ?? '';
-    $adresse_entreprise = $_POST['adresse_entreprise'] ?? '';
-    $raison_sociale = $_POST['raison_sociale'] ?? '';
+    $dateNaissance = $_POST['date_naissance'] ?? '';
+    $noSiren = $_POST['num_siren'] ?? '';
+    $idAdresse = $_POST['adresse_entreprise'] ?? '';
+    $raisonSocial = $_POST['raisonSocial'] ?? '';
 
     $sql = "INSERT INTO _vendeur 
-        (nom, prenom, email, tel, utilisateur, mdp, dateNaissance, confirmMdp, siren, adresse, sociale)
-        VALUES (:nom, :prenom, :email, :tel, :utilisateur, :mdp, :dateNaissance, :confirmMdp, :siren, :adresse, :sociale)";
+        (nom, prenom, email, tel, utilisateur, mdp, dateNaissance, siren, adresse, sociale)
+        VALUES (:nom, :prenom, :email, :tel, :utilisateur, :mdp, :dateNaissance, :siren, :adresse, :sociale)";
 
     $stmt = $pdo->prepare($sql);
 
     $stmt->execute([
-        ':nom' => $nom_contact,
-        ':prenom' => $prenom_contact,
+        ':nom' => $nom,
+        ':prenom' => $prenom,
         ':email' => $email,
-        ':tel' => $num_tel,
-        ':utilisateur' => $nom_utilisateur,
+        ':tel' => $noTelephone,
+        ':utilisateur' => $pseudo, 
         ':mdp' => $mdp,
-        ':dateNaissance' => $date_naissance,
-        ':confirmMdp' => $confimer_mdp,
-        ':siren' => $num_siren,
-        ':adresse' => $adresse_entreprise,
-        ':sociale' => $raison_sociale,
+        ':dateNaissance' => $dateNaissance,
+        ':siren' => $noSiren,
+        ':adresse' => $idAdresse,
+        ':sociale' => $raisonSocial,
     ]);
     }
 
