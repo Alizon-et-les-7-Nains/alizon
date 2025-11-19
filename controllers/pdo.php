17711a96a70ee3aebbfb15b98ca690e5224448ae
pperche <perche.paolo@gmail.com>
2025-11-19 15:16:13 +0100

    test pdo v3

--- controllers/pdo.php
+++ controllers/pdo.php
@@ -3,6 +3,6 @@ include('../../../config/config.php');
 try {
     $pdo = new PDO("$driver:host=$server;dbname=$dbname", $user, $pass);
 } catch (PDOException $e) {
-    die();
+    die("Erreur de connexion à la base de données.");
 }
 ?>
