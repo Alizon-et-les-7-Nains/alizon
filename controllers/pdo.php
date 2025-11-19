1d38864848bdbe372ac13c056568b769ac3e6c53
pperche <perche.paolo@gmail.com>
2025-11-19 15:15:48 +0100

    test pdo v2

--- controllers/pdo.php
+++ controllers/pdo.php
@@ -3,9 +3,6 @@ include('../../../config/config.php');
 try {
     $pdo = new PDO("$driver:host=$server;dbname=$dbname", $user, $pass);
 } catch (PDOException $e) {
-    print "Erreur !: " . $e->getMessage() . "<br/>";
     die();
 }
 ?>
-
-
