50a6ee06add208e358242a261d8f0299a4b1d200
marechalg <guewenmarechal@protonmail.com>
2025-11-19 14:19:48 +0100

    feat: Increase limit of recent orders displayed in backoffice query

--- queries/backoffice/dernieresCommandesProduit.sql
+++ queries/backoffice/dernieresCommandesProduit.sql
@@ -2,4 +2,4 @@ select quantiteCommande, dateCommande, p.idPanier, pro.idProduit, c.idCommande
 from _commande c join _panier p on c.idPanier = p.idPanier join _produitAuPanier pro on p.idPanier = pro.idPanier
 where pro.idProduit = :idProduit
 order by dateCommande desc
-limit 2;
+limit 4;
