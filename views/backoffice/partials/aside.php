d34d7ce2e208ae4933f2558dd89cc2db6ecc4ef9
marechalg <guewenmarechal@protonmail.com>
2025-11-20 10:10:47 +0100

    refactor aside menu: remove commented-out items and add captions for clarity

--- views/backoffice/partials/aside.php
+++ views/backoffice/partials/aside.php
@@ -1,52 +1,39 @@
 <aside class="backoffice">
     <ul>
+        <?php $class = __FILE__ == 'acceuil.php' ? 'here' : ''; ?>
         <li class="aside-btn here">
             <figure>
                 <img src="/public/images/homeDarkBlue.svg">
                 <figcaption>Accueil</figcaption>
             </figure>
         </li>
-        <!--
-        <li class="aside-btn">
-            <figure>
-                <img src="/public/images/courbeDark.svg">
-                <figcaption>Statistiques</figcaption>
-            </figure>
-        </li>
-        -->
+        <?php $class = __FILE__ == 'produits.php' ? 'here' : ''; ?>
         <li class="aside-btn">
             <figure>
                 <img src="/public/images/cartDarkBlue.svg">
                 <figcaption>Produits</figcaption>
             </figure>
         </li>
+        <?php $class = __FILE__ == 'stock.php' ? 'here' : ''; ?>
         <li class="aside-btn">
             <figure>
                 <img src="/public/images/boiteDark.svg">
                 <figcaption>Stock</figcaption>
             </figure>
         </li>
+        <?php $class = __FILE__ == 'avis.php' ? 'here' : ''; ?>
         <li class="aside-btn">
             <figure>
                 <img src="/public/images/chatDark.svg">
                 <figcaption>Avis</figcaption>
             </figure>
         </li>
+        <?php $class = __FILE__ == 'commandes.php' ? 'here' : ''; ?>
         <li class="aside-btn">
             <figure>
                 <img src="/public/images/cartCheckDark.svg">
                 <figcaption>Commandes</figcaption>
             </figure>
         </li>
-        <!--
-        <li class="aside-btn">
-            <figure>
-                <img src="/public/images/enveloppeDark.svg">
-                <figcaption>Retours</figcaption>
-            </figure>
-        </li>
-        -->
     </ul>
-
-    <script type="module" src="/public/script.js"></script>
 </aside>
