914ddb9b4987ac9bbf4e3936c16a6d1231a97eaf
marechalg <guewenmarechal@protonmail.com>
2025-11-20 13:27:08 +0100

    fix aside menu: correct class assignment for active state

--- views/backoffice/partials/aside.php
+++ views/backoffice/partials/aside.php
@@ -1,35 +1,35 @@
 <aside class="backoffice">
     <ul>
-        <?php $class = __FILE__ == 'acceuil.php' ? 'here' : ''; ?>
-        <li class="aside-btn here">
+        <?php $class = basename(__FILE__) == 'acceuil.php' ? 'here' : ''; ?>
+        <li class="aside-btn <?php echo $class; ?>">
             <figure>
                 <img src="/public/images/homeDarkBlue.svg">
                 <figcaption>Accueil</figcaption>
             </figure>
         </li>
-        <?php $class = __FILE__ == 'produits.php' ? 'here' : ''; ?>
-        <li class="aside-btn">
+        <?php $class = basename(__FILE__) == 'produits.php' ? 'here' : ''; ?>
+        <li class="aside-btn <?php echo $class; ?>">
             <figure>
                 <img src="/public/images/cartDarkBlue.svg">
                 <figcaption>Produits</figcaption>
             </figure>
         </li>
-        <?php $class = __FILE__ == 'stock.php' ? 'here' : ''; ?>
-        <li class="aside-btn">
+        <?php $class = basename(__FILE__) == 'stock.php' ? 'here' : ''; ?>
+        <li class="aside-btn <?php echo $class; ?>">
             <figure>
                 <img src="/public/images/boiteDark.svg">
                 <figcaption>Stock</figcaption>
             </figure>
         </li>
-        <?php $class = __FILE__ == 'avis.php' ? 'here' : ''; ?>
-        <li class="aside-btn">
+        <?php $class = basename(__FILE__) == 'avis.php' ? 'here' : ''; ?>
+        <li class="aside-btn <?php echo $class; ?>">
             <figure>
                 <img src="/public/images/chatDark.svg">
                 <figcaption>Avis</figcaption>
             </figure>
         </li>
-        <?php $class = __FILE__ == 'commandes.php' ? 'here' : ''; ?>
-        <li class="aside-btn">
+        <?php $class = basename(__FILE__) == 'commandes.php' ? 'here' : ''; ?>
+        <li class="aside-btn <?php echo $class; ?>">
             <figure>
                 <img src="/public/images/cartCheckDark.svg">
                 <figcaption>Commandes</figcaption>
