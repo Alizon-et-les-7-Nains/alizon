9dd5e6cb6109f99045d233466bce2fdfcbbe1dc4
Nosaphi <evan.legarrec56@gmail.com>
2025-11-20 08:52:28 +0100

    modif requete test 1

--- controllers/updateProduit.php
+++ controllers/updateProduit.php
@@ -5,7 +5,7 @@ session_start();
 $idProd = $_GET['id']; 
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $stmt = $pdo->prepare("UPDATE _produit SET nom = :nom, description = :description, prix = :prix, poids = :poids, mots_cles = :mots_cles WHERE idProd = :idProd");
+    $stmt = $pdo->prepare("UPDATE _produit SET nom = :nom, description = :description, prix = :prix, poids = :poids, mots_cles = :mot_cles WHERE idProd = :idProd");
     $img = $pdo->prepare("UPDATE _imageDeProduit SET URL = :url WHERE idProd = :idProd");
     $stmt->execute([
         ':nom' => $_POST['nom'],
