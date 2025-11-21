8ca33b177b75c8c6288adfc986c4bf594d387845
Zelirety <natuslanus@gmail.com>
2025-11-21 08:39:04 +0100

    resolution merge conflicts

--- views/scripts/frontoffice/paiement-types.ts
+++ views/scripts/frontoffice/paiement-types.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export interface CartItem {
   id: string;
   nom: string;
@@ -39,49 +38,3 @@ export interface OrderData {
   regionLivraison: string;
   numeroCarte: string;
 }
-=======
-// ============================================================================
-// TYPES & INTERFACES
-// ============================================================================
-
-export interface CartItem {
-  id: string;
-  nom: string;
-  prix: number;
-  qty: number;
-  img: string;
-}
-
-export interface Inputs {
-  adresseInput: HTMLInputElement | null;
-  codePostalInput: HTMLInputElement | null;
-  villeInput: HTMLInputElement | null;
-  numCarteInput: HTMLInputElement | null;
-  nomCarteInput: HTMLInputElement | null;
-  carteDateInput: HTMLInputElement | null;
-  cvvInput: HTMLInputElement | null;
-  recapEl: HTMLElement | null;
-}
-
-export interface ValidateAllParams {
-  inputs: Inputs;
-  departments: Map<string, string>;
-  postals: Map<string, Set<string>>;
-  cart: CartItem[];
-  selectedDepartment: { value: string | null };
-}
-
-export interface Maps {
-  departments: Map<string, string>;
-  citiesByCode: Map<string, Set<string>>;
-  postals: Map<string, Set<string>>;
-  allCities: Set<string>;
-}
-
-export interface OrderData {
-  adresseLivraison: string;
-  villeLivraison: string;
-  regionLivraison: string;
-  numeroCarte: string;
-}
->>>>>>> produitNathan
