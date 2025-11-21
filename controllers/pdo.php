5b137f3a946761250cbe962d56837f594359fad1
Nosaphi <evan.legarrec56@gmail.com>
2025-11-21 14:43:23 +0100

    connexion bd 2/2

--- controllers/pdo.php
+++ controllers/pdo.php
@@ -1,19 +1,8 @@
 <?php
 include('config.php');
 try {
-    $dbh = new PDO("$driver:host=$server;dbname=$dbname", 
-            $user, $pass);
-    foreach($dbh->query('SELECT * from saedb._client', 
-                        PDO::FETCH_ASSOC) 
-                as $row) {
-        echo "<pre>";
-        print_r($row);
-        echo "</pre>";
-    }
-    $dbh = null;
+    $pdo = new PDO("$driver:host=$server;dbname=$dbname", $user, $pass);
 } catch (PDOException $e) {
-    print "Erreur !: " . $e->getMessage() . "<br/>";
-    die();
+    die("Erreur PDO : " . $e->getMessage());
 }
 ?>
-
