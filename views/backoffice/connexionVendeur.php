f630825024553a34aebe06004ec9626c0dd16d35
lorasvlr <lora.lorinet@gmail.com>
2025-11-21 13:21:55 +0100

    creation branch

---
+++ views/backoffice/connexionVendeur.php
@@ -0,0 +1,27 @@
+<?php
+    require_once "../../controllers/pdo.php"; 
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../../public/style.css">
+    <title>Connexion à un compte vendeur</title>
+</head>
+<?php
+        require_once "./partials/header.php";
+?>
+<body>
+    <main class="connexionVendeur">
+        <img class="triskiel" src="../../public/images/triskiel gris.svg" alt="">
+        <div class="pdp_title">
+            <img src="../../public/images/pdp_user.svg" alt="photo de profil">
+            <h1>Connexion à votre compte vendeur Alizon</h1>
+        </div>
+    </main>
+</body>
+</html>
