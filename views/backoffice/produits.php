4266f4d1297224cd52b09b2f57f5a4b58b3a9958
Maaxxiimee <maximebourbon562@gmail.com>
2025-11-21 14:19:36 +0100

    Fix typo in image table name in produits.php

--- views/backoffice/produits.php
+++ views/backoffice/produits.php
@@ -1,6 +1,6 @@
 <?php 
     require_once '../../controllers/pdo.php';
-    $stmt = $pdo->query("SELECT prod.idproduit, nom, note, prix, url FROM _produit as prod JOIN _imagedeproduit as img on prod.idproduit = img.idproduit WHERE envente = true;");
+    $stmt = $pdo->query("SELECT prod.idproduit, nom, note, prix, url FROM _produit as prod JOIN _imageDeProduit as img on prod.idproduit = img.idproduit WHERE envente = true;");
     $produitEnVente = $stmt->fetchAll(PDO::FETCH_ASSOC); 
 ?>
 
