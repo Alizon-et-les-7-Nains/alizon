5337b713b1a2307f951361a964dcf0a3b66c308f
Maaxxiimee <maximebourbon562@gmail.com>
2025-11-21 09:13:48 +0100

    Fix typo in header redirection and clean up HTML formatting

--- controllers/session_start.php
+++ controllers/session_start.php
@@ -32,6 +32,6 @@
     session_start();  
     $id_session = session_id();
     $_SESSION['id_session'] = $id_session;
-    header('Location: ../views/frontoffice/acceuilConnecte.php');
+    header('Location: ../views/frontoffice/accueilConnecte.php');
     exit();
 ?>
