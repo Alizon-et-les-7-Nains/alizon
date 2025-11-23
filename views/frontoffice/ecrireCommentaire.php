99cb4dae012e2aef2b5c1d56c105e4d0dcfb536d
Zelirety <natuslanus@gmail.com>
2025-11-23 15:10:41 +0100

    gestion de l'insertion du message commentaire -> bdd // sans warnings

--- views/frontoffice/ecrireCommentaire.php
+++ views/frontoffice/ecrireCommentaire.php
@@ -2,14 +2,6 @@
 session_start();
 require_once "../../controllers/pdo.php";
 
-// Database connection
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-    die("Erreur de connexion : " . $e->getMessage());
-}
-
 // Insert review (only once!)
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     try {
