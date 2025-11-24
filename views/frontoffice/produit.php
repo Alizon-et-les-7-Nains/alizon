632c4a92ba76bb40c5f4e0b476153a16fe64fa15
Zelirety <natuslanus@gmail.com>
2025-11-25 00:20:34 +0100

    page telephone produit responsive ajustement

--- views/frontoffice/produit.php
+++ views/frontoffice/produit.php
@@ -220,12 +220,8 @@ if (isset($_SESSION['message_panier'])) {
         </div>
     </div>
     <img src="../../public/images/flecheDroite.svg" alt="Next" class="carousel-arrow next-arrow">
-</article>
+    </article>
     <article class="infoPreviewProduit">
-            <?php
-                // $note = 4.2; // Exemple de note moyenne A CHANGER
-                // $nombreAvis = 128; // Exemple de nombre d'avis A CHANGER
-            ?>
         <h1 class="nomProduit"><?php echo htmlspecialchars($produit['nom_produit']); ?></h1>
             <div class="product-rating">
                 <div>
@@ -261,20 +257,20 @@ if (isset($_SESSION['message_panier'])) {
         </div> -->
     </article>
     <article class="actionsProduit">
-    <h2>Vendu par <?php echo htmlspecialchars($produit['prenom_vendeur'] . ' ' . $produit['nom_vendeur']); ?></h2>
-    <p class="underline" id="plusDarticles"><a href="">Plus d'article de ce vendeur</a></p>
-    <br>
-    <hr>
-    <div class="ligneActions">
-        <img src="../../public/images/camion.png" alt="">
-        <p>Livraison <b>GRATUITE</b> - Expédié par <b>mondial relais</b>. Arrivée entre le <b>mar. 21 septembre - ven. 24 septembre</b></p>
-    </div>
-    <div class="ligneActions">
-        <img src="../../public/images/emplacement.png" alt="">
-        <p>Livré a <a href=""><b>Clermont-ferrand 63000</b>, 10 place saint-michel</a></p>   
-    </div>
-    <div class="ligneActions">
-        <img src="../../public/images/tec.png" alt="">
+        <h2>Vendu par <?php echo htmlspecialchars($produit['prenom_vendeur'] . ' ' . $produit['nom_vendeur']); ?></h2>
+        <p class="underline" id="plusDarticles"><a href="">Plus d'article de ce vendeur</a></p>
+        <br>
+        <hr>
+        <div class="ligneActions">
+            <img src="../../public/images/camion.png" alt="">
+            <p>Livraison <b>GRATUITE</b> - Expédié par <b>mondial relais</b>. Arrivée entre le <b>mar. 21 septembre - ven. 24 septembre</b></p>
+        </div>
+        <div class="ligneActions">
+            <img src="../../public/images/emplacement.png" alt="">
+            <p>Livré a <a href=""><b>Clermont-ferrand 63000</b>, 10 place saint-michel</a></p>   
+        </div>
+        <div class="ligneActions">
+            <img src="../../public/images/tec.png" alt="">
         <p>
             Consulter les <b><?php if (isset($_SESSION['user_id'])) {
             echo '<a href="legalesConnecte.php">conditions générales de vente</a>';
