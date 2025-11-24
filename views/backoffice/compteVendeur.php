cce76eaceaa1626bcc58438d40ea2d41c5341398
Timéo <timeodoom@gmail.com>
2025-11-24 18:06:19 +0100

    fix

--- views/backoffice/compteVendeur.php
+++ views/backoffice/compteVendeur.php
@@ -1,5 +1,3 @@
-[file name]: compteVendeur.php
-[file content begin]
 <?php
 session_start();
 require_once '../../controllers/pdo.php';
@@ -327,4 +325,3 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 </body>
 
 </html>
-[file content end]
