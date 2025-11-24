57f67b9d68e0f160fede14e0f6a2200a880a6cee
marechalg <guewenmarechal@protonmail.com>
2025-11-24 23:31:01 +0100

    fix: Wrap authentication checks in try-catch block to handle exceptions gracefully

--- controllers/auth.php
+++ controllers/auth.php
@@ -4,13 +4,18 @@ require_once 'pdo.php';
 
 session_start();
 
-$isValidSTMT = $pdo->prepare(file_get_contents('../queries/backoffice/connexion.sql'));
-$isValidSTMT->execute([':pseudo' => $_SESSION['pseudo'], ':mdp' => $_SESSION['mdp']]);
-$isValid = $isValidSTMT->fetchColumn();
+try {
+    $pdo->prepare();
+    $isValidSTMT = $pdo->prepare(file_get_contents('../queries/backoffice/connexion.sql'));
+    $isValidSTMT->execute([':pseudo' => $_SESSION['pseudo'], ':mdp' => $_SESSION['mdp']]);
+    $isValid = $isValidSTMT->fetchColumn();
 
-if (!$_SESSION['id_session'] || !$isValid) {
-    header('Location: ../views/backoffice/connexions.php');
-    die();
+    if (!$_SESSION['id_session'] || !$isValid) {
+        header('Location: ../views/backoffice/connexions.php');
+        die();
+    }
+} catch (Exception $e) {
+
 }
 
 ?>
