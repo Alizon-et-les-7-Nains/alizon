a5d83f4d55cc22052c719e9f865c122b8f87a351
Maaxxiimee <maximebourbon562@gmail.com>
2025-11-24 21:16:49 +0100

    try fix 3

--- controllers/creerPromotion.php
+++ controllers/creerPromotion.php
@@ -21,8 +21,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             }
         }
 
-        if (file_exists($photoPath)) {
-            unlink($photoPath);
+        if ($extension !== '') {
+            $oldFile = $photoPath . $extension;
+            if (file_exists($oldFile)) {
+                unlink($oldFile);
+            }
         }
 
         if (isset($_FILES['baniere']) && $_FILES['baniere']['tmp_name'] != '') {
@@ -33,7 +36,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         try {
             $dateSql = DateTime::createFromFormat('d/m/Y', $dateLimite)->format('Y-m-d');
-        } catch (error) {
+        } catch (Exception $e) {
             header('Location: ../views/backoffice/produits.php?error=1&idProduit='.$idProd.'.php');
             exit;
         }
