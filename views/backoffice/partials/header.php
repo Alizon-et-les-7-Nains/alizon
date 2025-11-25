3517c54e74b23e071aa6530c2872396c4bf1b8c0
pperche <perche.paolo@gmail.com>
2025-11-25 17:53:25 +0100

    redirection vers accueil v8

--- views/backoffice/partials/header.php
+++ views/backoffice/partials/header.php
@@ -1,5 +1,5 @@
 <header class="backoffice">
-    <a href="/public/views/backoffice/acceuil.php">
+    <a href="/views/backoffice/acceuil.php">
     <div>
         <img src="/public/images/logoBackoffice.svg">
         <h1>Alizon</h1>
