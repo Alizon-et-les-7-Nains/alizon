c0dee142cbdc9c9bd116baa8a544690104a2f570
Nosaphi <evan.legarrec56@gmail.com>
2025-11-25 12:03:14 +0100

    _SESSION fix ligne id

--- controllers/session_start.php
+++ controllers/session_start.php
@@ -31,7 +31,7 @@
 
     session_start();  
     $id_session = session_id();
-    $_SESSION['id_session'] = $id_session;
+    $_SESSION['user_id'] = $id_session;
     header('Location: ../views/frontoffice/accueilConnecte.php');
     exit();
 ?>
