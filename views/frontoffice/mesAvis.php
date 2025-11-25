36cd6c5abdcac8d0ca399fd890d96d462f7d641d
Nosaphi <evan.legarrec56@gmail.com>
2025-11-25 14:58:28 +0100

    test 3

--- views/frontoffice/mesAvis.php
+++ views/frontoffice/mesAvis.php
@@ -9,32 +9,41 @@ if (!isset($_SESSION['user_id'])) {
 
 $id_client = $_SESSION['user_id'];
 
+// Récupérer les avis du client
 $stmt = $pdo->query("SELECT * FROM _avis WHERE idClient = $id_client");
 $mesAvis = $stmt->fetchAll(PDO::FETCH_ASSOC);
-var_dump($mesAvis);
-exit;
-
 ?>
-
 
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Modifier Avis</title>
-
-    <link rel="icon" href="/public/images/logoBackoffice.svg">
     <link rel="stylesheet" href="../../public/style.css">
 </head>
-<body class="modifierAvis">
-    <header>
-        <?php include './partials/headerConnecte.php'; ?>
-    </header>
-    <main>
-        <h1> Mes Commentaires</h1>
+<body>
 
-        
-    </main>
+<h1>Mes Commentaires</h1>
 
-</body>
+<?php
+if (count($mesAvis) === 0) {
+    echo "<h2>Aucun avis</h2>";
+}
+
+foreach ($mesAvis as $avis) {
+    
+    // Charger la requête SQL d'images
+    $sql = file_get_contents('../../queries/imagesAvis.sql');
+
+    // Remplacer les variables
+    $sql = str_replace('$idProduit', $avis['idProduit'], $sql);
+    $sql = str_replace('$idClient', $avis['idClient'], $sql);
+
+    // Récupérer les images
+    $imagesAvis = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+
+    echo "<pre>";
+    print_r($imagesAvis);
+    echo "</pre>";
+}
+?>
