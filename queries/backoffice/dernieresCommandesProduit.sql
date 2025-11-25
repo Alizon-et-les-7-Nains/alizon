97b5f53646b04ff7822c0c115a22b323bbd3d353
marechalg <guewenmarechal@protonmail.com>
2025-11-25 22:20:34 +0100

    Remove vendor ID from recent orders SQL query and update execution in stock views

--- queries/backoffice/dernieresCommandesProduit.sql
+++ queries/backoffice/dernieresCommandesProduit.sql
@@ -1,5 +1,5 @@
-select quantiteCommande, dateCommande, p.idPanier, pro.idProduit, c.idCommande, pro.idVendeur
+select quantiteCommande, dateCommande, p.idPanier, pro.idProduit, c.idCommande
 from _commande c join _panier p on c.idPanier = p.idPanier join _produitAuPanier pro on p.idPanier = pro.idPanier
-where pro.idProduit = :idProduit and pro.idVendeur = :idVendeur
+where pro.idProduit = :idProduit
 order by dateCommande desc
 limit 3;
