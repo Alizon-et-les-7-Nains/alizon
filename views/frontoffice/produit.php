42fe331d988bb9bab8e727fcfaf89dd74934b479
Zelirety <natuslanus@gmail.com>
2025-11-25 10:13:41 +0100

    promo image + texte position v6

--- views/frontoffice/produit.php
+++ views/frontoffice/produit.php
@@ -223,7 +223,6 @@ $promotion = calculerPromotion($produit);
 } ?>
 </header>
 <main>
-<main>
 <?php
 // Afficher les messages de confirmation
 if (isset($_SESSION['message_panier'])) {
@@ -237,7 +236,9 @@ if (isset($_SESSION['message_panier'])) {
 <article class="rectangleProduit">
     <div class="banniere">
         <h2><?php echo htmlspecialchars($promotion['taux_remise']); ?></h2>
-        <img src="../../public/images/laBanniere.png" alt="">
+        <img class="poly" src="../../public/images/poly1.svg" alt="">
+        <img class="imgBanniere" src="../../public/images/laBanniere.png" alt="">
+        <img class="poly" src="../../public/images/poly2.svg" alt="">
     </div>
     <img src="../../public/images/flecheGauche.svg" alt="Previous" class="carousel-arrow prev-arrow">
     <div class="carousel-container">
