723608ffe986fafb420b9f93e07d9c3880ab8d39
Nosaphi <evan.legarrec56@gmail.com>
2025-11-25 17:06:24 +0100

    fin supprimer produit + fin page générale + css

--- views/frontoffice/mesAvis.php
+++ views/frontoffice/mesAvis.php
@@ -1,5 +1,7 @@
 <?php 
 require_once "../../controllers/pdo.php";
+require_once "../../controllers/date.php";
+
 session_start();
 
 if (!isset($_SESSION['user_id'])) {
@@ -12,6 +14,15 @@ $id_client = $_SESSION['user_id'];
 $stmt = $pdo->query("SELECT * FROM _avis WHERE idClient = $id_client");
 $mesAvis = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+function afficherEtoiles($note) {
+    $html = "";
+    for ($i = 1; $i <= 5; $i++) {
+        if ($i <= $note) $html .= "<img class='etoile' src='/public/images/etoile.svg'>";
+        else $html .= "<img class='vide' src='/public/images/etoileVide.svg'>";
+    }
+    return $html;
+}
+
 ?>
 
 
@@ -20,7 +31,7 @@ $mesAvis = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Modifier Avis</title>
+    <title>Mes Avis</title>
 
     <link rel="icon" href="/public/images/logoBackoffice.svg">
     <link rel="stylesheet" href="../../public/style.css">
@@ -29,46 +40,63 @@ $mesAvis = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <header>
         <?php include './partials/headerConnecte.php'; ?>
     </header>
-    <main>
+    <main class="mesAvis">
         <h1> Mes Commentaires</h1>
 
         <?php
-            if (count($mesAvis) == 0) echo "<h2>Aucun avis</h2>";
-            foreach ($mesAvis as $avis) {
-                $imagesAvis = ($pdo->query(str_replace('$idClient', $avis['idClient'], str_replace('$idProduit', $avis['idProduit'], file_get_contents('../../queries/imagesAvis.sql')))))->fetchAll(PDO::FETCH_ASSOC);
-                $imageClient = "/images/photoProfilClient/photo_profil" . $avis['idClient'] . ".svg";
-                $html = "
-                <table>
-                    <tr>
-                        <th rowspan=2>
-                            <figure>
-                                <img src='$imageClient' onerror=" . '"this.style.display=' . "'none'" . '"' . ">
-                                <figcaption>" . $avis['pseudo'] . "</figcaption>
-                            </figure>
-                            <figure>
-                                <figcaption>" . str_replace('.', ',', $avis['note']) . "</figcaption>
-                                <img src='/public/images/etoile.svg'>
-                            </figure>
-                        </th>
-                        <th>" . $avis['nomProduit'] . " - " . $avis['titreAvis'] . "</th>
-                        <td>Le" . formatDate($avis['dateAvis']) . "</td>
-                    </tr>
-                    <tr>
-                        <td colspan='2'>" . $avis['contenuAvis'] . "</td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td colspan='2'>";   
-                            foreach ($imagesAvis as $imageAvi) {
-                                $html .= "<img src='" . $imageAvi['URL'] . "' class='imageAvis'>";
-                            }
-                        $html .= "</td>
-                    </tr>
-                </table>
-                ";
-                echo $html;
+            if (!$mesAvis){
+                ?> <h2> Aucun Avis </h2> <?php
             }
+            else{
+                ?>
+                <section> <?php
+                foreach ($mesAvis as $avis) {
+                $p = $avis['idProduit'];
+                $stmt2 = $pdo->query("SELECT * FROM _produit WHERE idProduit = $p");
+                $monProduit = $stmt2->fetch(PDO::FETCH_ASSOC);
+                $stmt3 = $pdo->query("SELECT * FROM _imageDeProduit WHERE idProduit = $p");
+                $imageProduit = $stmt3->fetch(PDO::FETCH_ASSOC); 
+                ?> 
+
+                <article>
+                    <div class="produit">
+                        <img src="/image/<?php echo($imageProduit['URL']) ?>">
+
+                        <div class="infos-produit">
+                            <h3><?php echo($monProduit['nom']); ?></h3>
+                            <p><?php echo($monProduit['prix'] . "€"); ?></p>
+                        </div>
+                    </div>
+
+
+                    <div class="contenu">
+
+                        <div class="header-contenu">
+                            <h2><?php echo($avis['titreAvis']); ?></h2>
+                            <span class="date"><?php echo("Publié le " . formatDate($avis['dateAvis'])); ?></span>
+                        </div>
+                        <div class="note">
+                            <?= afficherEtoiles(round($avis['note'])); ?>
+                        </div>
+                        <div class="texte">
+                            <p><?php echo($avis['contenuAvis']); ?></p>
+                        </div>
+
+                        <div class="actions">
+                            <a href="../../controllers/modifierAvis.php<?php echo($p);?>">Modifier</a>
+                            <a href="../../controllers/supprimerAvis.php?id=<?php echo($p);?>" class="supprimerAvis">Supprimer</a>
+                        </div>
+
+                    </div>
+
+            </article>
+
+                <?php
+                
+            }  
+        }      
 ?>
+            </section>
     </main>
     <?php include './partials/footerConnecte.php'; ?>
 </body>
