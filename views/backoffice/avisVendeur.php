e0176a0b2863ec11ba48c802a170739929b8ce52
Timéo <timeodoom@gmail.com>
2025-11-25 09:57:48 +0100

    fix: Correct client image path and improve display of client information in avisVendeur.php

--- views/backoffice/avisVendeur.php
+++ views/backoffice/avisVendeur.php
@@ -73,6 +73,7 @@ $avis = $stmt->fetchAll(PDO::FETCH_ASSOC);
                             )
                         )->fetchAll(PDO::FETCH_ASSOC);
 
+                        // Correction du chemin de l'image du client
                         $imageClient = "/public/images/photoProfilClient/photo_profil" . $avi['idClient'] . ".svg";
                     ?>
 
@@ -81,34 +82,38 @@ $avis = $stmt->fetchAll(PDO::FETCH_ASSOC);
                         <th rowspan="3" class="col-gauche">
                             <figure class="profil-client">
                                 <img src="<?= $imageClient ?>" onerror="this.style.display='none'">
-                                <figcaption><?= $avi['pseudo'] ?></figcaption>
+                                <figcaption>
+                                    <?= isset($avi['pseudo']) ? $avi['pseudo'] : $avi['prenom'] . ' ' . $avi['nomClient'] ?>
+                                </figcaption>
+                            </figure>
+                            <figure class="etoiles">
+                                <figcaption><?= str_replace('.', ',', $avi['note']) ?>/5</figcaption>
+                                <img src="/public/images/etoile.svg">
                             </figure>
                         </th>
 
                         <td class="ligne">
-                            <figure class="etoiles">
-                                <figcaption><?= str_replace('.', ',', $avi['note']) ?></figcaption>
-                                <img src="/public/images/etoile.svg">
-                            </figure>
-                            <?= $avi['titreAvis'] ?> - <?= $avi['nomProduit'] ?>
+                            <strong><?= $avi['titreAvis'] ?></strong> - <?= $avi['nomProduit'] ?>
                         </td>
                         <td class="ligne">
-                            <p class=" date-avis">Avis déposé le <?= formatDate($avi['dateAvis']) ?></p>
+                            <p class="date-avis">Avis déposé le <?= formatDate($avi['dateAvis']) ?></p>
                         </td>
 
                     </tr>
 
                     <tr>
-                        <td class=" ligne text" colspan="2">
+                        <td class="ligne text" colspan="2">
                             <?= $avi['contenuAvis'] ?>
                         </td>
                     </tr>
 
                     <tr>
                         <td class="ligne" colspan="2">
+                            <?php if (!empty($imagesAvis)): ?>
                             <?php foreach ($imagesAvis as $imageAvi): ?>
-                            <img src="<?= $imageAvi['URL'] ?>" class="imageAvis">
+                            <img src="<?= $imageAvi['URL'] ?>" class="imageAvis" style="max-width: 100px; margin: 5px;">
                             <?php endforeach; ?>
+                            <?php endif; ?>
                         </td>
                     </tr>
                 </table>
