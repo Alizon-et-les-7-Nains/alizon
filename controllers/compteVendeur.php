37b15b4aa225c9400d7b30d76e365ac63b920667
pperche <perche.paolo@gmail.com>
2025-11-25 16:33:47 +0100

    debug

--- controllers/compteVendeur.php
+++ controllers/compteVendeur.php
@@ -101,7 +101,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     } catch (Exception $e) {
         $pdo->rollBack();
-        header("Location: ../backoffice/compteVendeur.php?error=1");
+        echo "Erreur SQL : " . $e->getMessage();
         exit();
     }
 }
