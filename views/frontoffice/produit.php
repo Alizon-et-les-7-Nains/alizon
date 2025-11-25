87107bbe90c9a9697dad3838acb18585ce102c5d
Zelirety <natuslanus@gmail.com>
2025-11-25 16:28:56 +0100

    ajout fonctionnalité like/dislike v5***

--- views/frontoffice/produit.php
+++ views/frontoffice/produit.php
@@ -2,6 +2,70 @@
 include '../../controllers/pdo.php';
 session_start();
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['action']) && $_POST['action'] === 'voter_avis') {
+    if (isset($_SESSION['user_id'])) {
+        $idClient = $_SESSION['user_id'];
+        $idProduit = intval($_POST['idProduit']);
+        $idClientAvis = intval($_POST['idClientAvis']);
+        $typeVote = $_POST['type']; // 'like' ou 'dislike'
+        
+        $keyVote = "vote_{$idProduit}_{$idClientAvis}_{$idClient}";
+        
+        $votePrecedent = $_SESSION[$keyVote] ?? null;
+        
+        if ($votePrecedent === $typeVote) {
+            unset($_SESSION[$keyVote]);
+        } else {
+            $_SESSION[$keyVote] = $typeVote;
+        }
+        
+        header("Location: ?id=" . $productId);
+        exit;
+    }
+}
+
+function compterVotesAvis($idProduit, $idClientAvis) {
+    $likes = 0;
+    $dislikes = 0;
+    
+    foreach ($_SESSION as $key => $value) {
+        if (strpos($key, "vote_{$idProduit}_{$idClientAvis}_") === 0) {
+            if ($value === 'like') {
+                $likes++;
+            } elseif ($value === 'dislike') {
+                $dislikes++;
+            }
+        }
+    }
+    
+    return ['likes' => $likes, 'dislikes' => $dislikes];
+}
+
+function getVoteUtilisateur($idProduit, $idClientAvis) {
+    if (!isset($_SESSION['user_id'])) {
+        return null;
+    }
+    
+    $idClient = $_SESSION['user_id'];
+    $keyVote = "vote_{$idProduit}_{$idClientAvis}_{$idClient}";
+    
+    return $_SESSION[$keyVote] ?? null;
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['action']) && $_POST['action'] === 'ajouter_panier') {
+    $idProduit = intval($_POST['idProduit']);
+    $quantite = intval($_POST['quantite']);
+    
+    if (isset($_SESSION['user_id'])) {
+        $idClient = $_SESSION['user_id'];
+                $success = updateQuantityInDatabase($pdo, $idClient, $idProduit, $quantite);
+    }
+    if ($success) {
+        $_SESSION['message_panier'] = "Produit ajouté au panier avec succès!";
+    } else {
+        $_SESSION['message_panier'] = "Erreur lors de l'ajout au panier.";
+    }
+}
 
 $productId = intval($_GET['id']) ?? 0;
 
@@ -423,37 +487,8 @@ if (isset($_SESSION['message_panier'])) {
                              <?php endforeach; ?>
                         </div>   
                         <div class="actionsAvis">
-                            <div class="actionsAvis">
-                            <form method="POST" style="display: inline;">
-                                <input type="hidden" name="action" value="voter_avis">
-                                <input type="hidden" name="idProduit" value="<?php echo $productId; ?>">
-                                <input type="hidden" name="idClientAvis" value="<?php echo $avis['idClient']; ?>">
-                                <input type="hidden" name="type" value="like">
-                                
-                                <?php
-                                $votesAvis = compterVotesAvis($productId, $avis['idClient']);
-                                $voteUtilisateur = getVoteUtilisateur($productId, $avis['idClient']);
-                                $likeActive = ($voteUtilisateur === 'like') ? 'pouceHautActive.png' : 'pouceHaut.png';
-                                $dislikeActive = ($voteUtilisateur === 'dislike') ? 'pouceBasActive.png' : 'pouceBas.png';
-                                ?>
-                                
-                                <button type="submit" class="btn-vote" style="background: none; border: none; cursor: pointer;">
-                                    <img src="../../public/images/<?php echo $likeActive; ?>" alt="Like" class="pouce">
-                                    <span class="count-likes"><?php echo $votesAvis['likes']; ?></span>
-                                </button>
-                            </form>
-                            
-                            <form method="POST" style="display: inline;">
-                                <input type="hidden" name="action" value="voter_avis">
-                                <input type="hidden" name="idProduit" value="<?php echo $productId; ?>">
-                                <input type="hidden" name="idClientAvis" value="<?php echo $avis['idClient']; ?>">
-                                <input type="hidden" name="type" value="dislike">
-                                
-                                <button type="submit" class="btn-vote" style="background: none; border: none; cursor: pointer;">
-                                    <img src="../../public/images/<?php echo $dislikeActive; ?>" alt="Dislike" class="pouce">
-                                    <span class="count-dislikes"><?php echo $votesAvis['dislikes']; ?></span>
-                                </button>
-                            </form>
+                            <img src="../../public/images/pouceHaut.png" alt="Like" onclick="changerPouce(this, 'haut')" class="pouce">
+                            <img src="../../public/images/pouceBas.png" alt="Dislike" onclick="changerPouce(this, 'bas')" class="pouce">
                             <shape></shape>
                             <a href="#">Signaler</a>
                         </div>
