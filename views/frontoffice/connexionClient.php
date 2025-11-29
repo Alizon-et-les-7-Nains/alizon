a3b8d2b67ea6c83ffde504d19afac611719a10e5
Nosaphi <evan.legarrec56@gmail.com>
2025-11-29 14:00:27 +0100

    modif redirection accueil page connexion (accueilConnecte -> deconnecte)

--- views/frontoffice/connexionClient.php
+++ views/frontoffice/connexionClient.php
@@ -82,7 +82,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <div class="headerMain">
             <div class="logoNom">
                 <img src="../../../public/images/logoAlizonHeader.png" alt="Logo Alizon">
-                <h1><a href="../frontoffice/accueilConnecte.php"><b>Alizon</b></a></h1>
+                <h1><a href="../frontoffice/accueilDeconnecte.php"><b>Alizon</b></a></h1>
             </div>
         </div>
 
