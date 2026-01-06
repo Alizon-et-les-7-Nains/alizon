5b2be8897c2d8e6973e1417649ef6b1d2292599d
Maaxxiimee <maximebourbon562@gmail.com>
2026-01-06 17:39:56 +0100

    Refactor header templates and update styles
    - Updated headerConnecte.php to uncomment the search bar section.
    - Modified headerDeconnecte.php to include a new carousel section displaying categories fetched from the database.
    - Cleaned up the search bar section in headerDeconnecte.php.
    - Adjusted styles in _commandes.scss by removing unnecessary font-weight and ensuring consistent padding and box-sizing for input fields.
    - Added cursor pointer style for better user interaction feedback.

--- views/frontoffice/partials/headerDeconnecte.php
+++ views/frontoffice/partials/headerDeconnecte.php
@@ -5,14 +5,14 @@
                 <img src="../../../public/images/logoAlizonHeader.png" alt="Logo Alizon">
                 <h1><a href="../frontoffice/accueilDeconnecte.php"><b>Alizon</b></a></h1>
             </div>
-        <div class="searchBar">
+        </div>
 
-        <!-- <div class="searchBar">
+        <div class="searchBar">
             <div class="search-wrapper">
                 <i class="bi bi-search"></i>
                 <input type="search" name="recherche" id="searchbar" placeholder="Rechercher">
             </div>
-        </div> -->
+        </div>
 
         </div>
             <div class="icons">
@@ -21,6 +21,16 @@
                     <a href="../frontoffice/connexionClient.php"><img src="../../../public/images/utilLightBlue.svg" alt=""></a>
                     <p>Se connecter</p>
                 </div>
+            </div>
+        </div>
+
+        <div class="carousel">
+            <div class="group">
+                <?php 
+                    $categorie = ($pdo->query("SELECT * FROM _categorie"))->fetchAll(PDO::FETCH_ASSOC);
+                    foreach ($categorie as $value) { ?>
+                        <a class="categorie"><?php echo $value['nomCategorie']; ?></a>
+                <?php } ?>
             </div>
         </div>
 
