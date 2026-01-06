28192f5ca604a7e4bfd0464396c8739e7dfa3fea
Maaxxiimee <maximebourbon562@gmail.com>
2026-01-06 17:42:22 +0100

    Refactor header layout for improved structure and readability

--- views/frontoffice/partials/headerDeconnecte.php
+++ views/frontoffice/partials/headerDeconnecte.php
@@ -1,25 +1,27 @@
 <header class="headerFront">
 
-        <div class="headerMain">
-            <div class="logoNom">
-                <img src="../../../public/images/logoAlizonHeader.png" alt="Logo Alizon">
-                <h1><a href="../frontoffice/accueilDeconnecte.php"><b>Alizon</b></a></h1>
+        <div style="display: flex;">
+            <div class="headerMain">
+                <div class="logoNom">
+                    <img src="../../../public/images/logoAlizonHeader.png" alt="Logo Alizon">
+                    <h1><a href="../frontoffice/accueilDeconnecte.php"><b>Alizon</b></a></h1>
+                </div>
             </div>
-        </div>
 
-        <div class="searchBar">
-            <div class="search-wrapper">
-                <i class="bi bi-search"></i>
-                <input type="search" name="recherche" id="searchbar" placeholder="Rechercher">
+            <div class="searchBar">
+                <div class="search-wrapper">
+                    <i class="bi bi-search"></i>
+                    <input type="search" name="recherche" id="searchbar" placeholder="Rechercher">
+                </div>
             </div>
-        </div>
 
-        </div>
-            <div class="icons">
-                <a href="../frontoffice/panierDeconnecte.php"><img src="../../../public/images/cartLightBlue.svg" alt=""></a>
-                <div class="seConnecter">
-                    <a href="../frontoffice/connexionClient.php"><img src="../../../public/images/utilLightBlue.svg" alt=""></a>
-                    <p>Se connecter</p>
+            </div>
+                <div class="icons">
+                    <a href="../frontoffice/panierDeconnecte.php"><img src="../../../public/images/cartLightBlue.svg" alt=""></a>
+                    <div class="seConnecter">
+                        <a href="../frontoffice/connexionClient.php"><img src="../../../public/images/utilLightBlue.svg" alt=""></a>
+                        <p>Se connecter</p>
+                    </div>
                 </div>
             </div>
         </div>
