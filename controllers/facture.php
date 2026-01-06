4e29a2e9a9c446bbceb82e26d4b41bb9d50bdd32
Nosaphi <evan.legarrec56@gmail.com>
2026-01-06 16:29:16 +0100

    add

--- controllers/facture.php
+++ controllers/facture.php
@@ -1,5 +1,5 @@
 <?php 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '../vendor/autoload.php';
 require_once './pdo.php';
 $idCommande = $_GET['id'];
 use Dompdf\Dompdf;
