4b225a818e433cb52c52848428e2ea831a64746e
marechalg <guewenmarechal@protonmail.com>
2026-01-09 14:05:45 +0100

    Fixed table name in prodsDeCommande.sql

--- queries/backoffice/commandes/prodsDeCommande.sql
+++ queries/backoffice/commandes/prodsDeCommande.sql
@@ -1,4 +1,4 @@
 select unique idPanier, p.idProduit, quantiteProduit, p.nom, p.prix
-from _commande natural join _produitaupanier pp join _produit p on p.idProduit = pp.idProduit 
+from _commande natural join _produitAuPanier pp join _produit p on p.idProduit = pp.idProduit 
 where idCommande = ? and p.idVendeur = ?
 order by quantiteProduit desc;
