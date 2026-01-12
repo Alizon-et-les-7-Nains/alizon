ba5b40c8e21de53313a019877f8e5e813efe5b25
Nosaphi <evan.legarrec56@gmail.com>
2026-01-12 11:38:50 +0100

    fix 1 filtre note

--- views/frontoffice/catalogue.php
+++ views/frontoffice/catalogue.php
@@ -3,7 +3,7 @@ include "../../controllers/pdo.php";
 include "../../controllers/prix.php";
 session_start();
 
-$produitsParPage = 5;
+$produitsParPage = 15;
 $page = isset($_GET['page']) ? max(1, (int)$_GET['page']) : 1;
 $offset = ($page - 1) * $produitsParPage;
 
