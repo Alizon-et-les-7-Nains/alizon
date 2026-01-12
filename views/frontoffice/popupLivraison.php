5b4d436ab833244201fe7aeaf1a7b276f842f733
pperche <perche.paolo@gmail.com>
2026-01-12 17:14:03 +0100

    Add link to delivery tracking and create popupLivraison.php

---
+++ views/frontoffice/popupLivraison.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <button popovertarget="mypopover">Open popOver</button>
+    <div popover="auto" id="mypopover">
+      <h1>PopOver</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam,
+        excepturi.
+      </p>
+      <button>Test</button>
+    </div>
+  </body>
+</html>
