6918634a2cfe3d8a4b4abcaae0cece7eed8eabd4
lorasvlr <lora.lorinet@gmail.com>
2026-01-13 17:30:31 +0100

    Rajout de style pour ne pas bloquer le btn option dans le stock

--- public/scriptNotif.js
+++ public/scriptNotif.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     const urlParams = new URLSearchParams(window.location.search);
     const productId = urlParams.get('reassort_id');
-    
+
     if (productId) {
+        document.querySelectorAll('dialog[open]').forEach(d => d.close());
+
         const targetDialog = document.getElementById('d-' + productId);
-        
+
         if (targetDialog) {
             targetDialog.show();
             targetDialog.scrollIntoView({ behavior: 'smooth', block: 'center' });
