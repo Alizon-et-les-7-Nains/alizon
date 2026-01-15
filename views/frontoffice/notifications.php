c6780fa252b9f710918933ce9722a66dff01a18e
Maaxxiimee <maximebourbon562@gmail.com>
2026-01-15 09:55:35 +0100

    Refactor notifications display logic and styles
    - Introduced a new function `getNotifications` to fetch notifications from the database based on client ID and seller status.
    - Updated the notifications display to dynamically render notification titles, content, and dates.
    - Removed hardcoded notification examples for a more dynamic approach.
    - Adjusted SCSS styles for better responsiveness and layout consistency.

--- views/frontoffice/notifications.php
+++ views/frontoffice/notifications.php
@@ -11,6 +11,27 @@ if (!isset($_SESSION['user_id'])) {
 
 $id_client = $_SESSION['user_id'];
 
+function getNotifications($pdo, $idClient, $is_vendeur) {
+    $sql = "SELECT * FROM _notification 
+            WHERE idClient = :idClient 
+            AND is_vendeur = :is_vendeur 
+            ORDER BY dateNotif DESC 
+            LIMIT 1";
+            
+    $stmt = $pdo->prepare($sql);
+
+    $stmt->execute([
+        'idClient'   => $idClient,
+        'is_vendeur' => $is_vendeur
+    ]);
+
+    $notif = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+    return $notif; 
+}
+
+$notifs = getNotifications($pdo, $id_client, 0)
+
 ?>
 
 <!DOCTYPE html>
@@ -33,97 +54,26 @@ $id_client = $_SESSION['user_id'];
 
         <section class="ensembleNotif">
             <div class="sidebarNotif">
+            <?php foreach($notifs as $notif) { ?>
                 <div class="apercuNotif">
                     <div>
                         <img src="../../public/images/bellRingDark.svg" alt="Nouvelle notification">
                     </div>
                     <div>
-                        <h3>Votre commande à été livrée</h3>
-                        <h4>Votre commande "Cidre brut breton" a été livrée au 19 Rue de Cerbar, 56300 Pluvigner le 29/10/2026 à 12h56</h4>
-                        <h5>Il y a 59 minutes</h5>
+                        <h3><?= $notif['titreNotif'] ?></h3>
+                        <h4><?= $notif['contenuNotif'] ?></h4>
+                        <h5><?= $notif['dateNotif'] ?></h5>
                     </div>
                 </div>
-                <div class="apercuNotif">
-                    <div>
-                        <img src="../../public/images/bellRingDark.svg" alt="Nouvelle notification">
-                    </div>
-                    <div>
-                        <h3>Votre commande à été livrée</h3>
-                        <h4>Votre commande "Cidre brut breton" a été livrée au 19 Rue de Cerbar, 56300 Pluvigner le 29/10/2026 à 12h56</h4>
-                        <h5>Il y a 59 minutes</h5>
-                    </div>
-                </div>
-                        <div class="apercuNotif">
-                    <div>
-                        <img src="../../public/images/bellRingDark.svg" alt="Nouvelle notification">
-                    </div>
-                    <div>
-                        <h3>Votre commande à été livrée</h3>
-                        <h4>Votre commande "Cidre brut breton" a été livrée au 19 Rue de Cerbar, 56300 Pluvigner le 29/10/2026 à 12h56</h4>
-                        <h5>Il y a 59 minutes</h5>
-                    </div>
-                </div>
-                <div class="apercuNotif">
-                    <div>
-                        <img src="../../public/images/bellRingDark.svg" alt="Nouvelle notification">
-                    </div>
-                    <div>
-                        <h3>Votre commande à été livrée</h3>
-                        <h4>Votre commande "Cidre brut breton" a été livrée au 19 Rue de Cerbar, 56300 Pluvigner le 29/10/2026 à 12h56</h4>
-                        <h5>Il y a 59 minutes</h5>
-                    </div>
-                </div>
-                        <div class="apercuNotif">
-                    <div>
-                        <img src="../../public/images/bellRingDark.svg" alt="Nouvelle notification">
-                    </div>
-                    <div>
-                        <h3>Votre commande à été livrée</h3>
-                        <h4>Votre commande "Cidre brut breton" a été livrée au 19 Rue de Cerbar, 56300 Pluvigner le 29/10/2026 à 12h56</h4>
-                        <h5>Il y a 59 minutes</h5>
-                    </div>
-                </div>
-                <div class="apercuNotif">
-                    <div>
-                        <img src="../../public/images/bellRingDark.svg" alt="Nouvelle notification">
-                    </div>
-                    <div>
-                        <h3>Votre commande à été livrée</h3>
-                        <h4>Votre commande "Cidre brut breton" a été livrée au 19 Rue de Cerbar, 56300 Pluvigner le 29/10/2026 à 12h56</h4>
-                        <h5>Il y a 59 minutes</h5>
-                    </div>
-                </div>
-                <div class="apercuNotif">
-                    <div>
-                        <img src="../../public/images/bellRingDark.svg" alt="Nouvelle notification">
-                    </div>
-                    <div>
-                        <h3>Votre commande à été livrée</h3>
-                        <h4>Votre commande "Cidre brut breton" a été livrée au 19 Rue de Cerbar, 56300 Pluvigner le 29/10/2026 à 12h56</h4>
-                        <h5>Il y a 59 minutes</h5>
-                    </div>
-                </div>
-                <div class="apercuNotif">
-                    <div>
-                        <img src="../../public/images/bellRingDark.svg" alt="Nouvelle notification">
-                    </div>
-                    <div>
-                        <h3>Votre commande à été livrée</h3>
-                        <h4>Votre commande "Cidre brut breton" a été livrée au 19 Rue de Cerbar, 56300 Pluvigner le 29/10/2026 à 12h56</h4>
-                        <h5>Il y a 59 minutes</h5>
-                    </div>
-                </div>
+            <?php } ?>
             </div>
             <article class="ecranNotif">
                 <div class="titleNotif">
-                    <h1>Votre commande à été livrée</h1>
-                    <h3>Il y a 16 minutes</h3>
+                    <h1>X</h1>
+                    <h3>X</h3>
                 </div>
                 <div class="contenuNotif">
-                    Votre commande “Rillettes de thon - 300g” a été livrée au 19 Rue de Cerbar, 56300 Pluvigner ce lundi 29 janvier. Notre service de livraison vous informe que le colis a bien été déposé à l’adresse indiquée et qu’il est désormais disponible à votre domicile. Le livreur a pris soin de déposer le paquet dans un endroit sécurisé afin de garantir la fraîcheur et la qualité du produit.
-                    Vous pouvez déguster vos rillettes de thon dès à présent. Préparées avec soin, elles sont idéales pour accompagner un apéritif entre amis, garnir des toasts croustillants ou encore relever une salade composée. Conservez le produit au réfrigérateur et consommez-le rapidement après ouverture afin d’apprécier pleinement toutes ses saveurs.
-                    Si vous n’avez pas reçu la commande ou si vous constatez un problème avec le produit, n’hésitez pas à contacter notre service client via votre espace personnel. Nous serons ravis de vous assister et, si nécessaire, de procéder à un remplacement ou à un remboursement.
-                    Merci de votre confiance et bonne dégustation !
+                    X
                 </div>
             </article>
         </section>
