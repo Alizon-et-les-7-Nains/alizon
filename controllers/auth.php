c6ff1a9ded3a645544d6e20225eb4eb2f242681e
Nosaphi <evan.legarrec56@gmail.com>
2026-01-15 12:04:11 +0100

    test vardump auth + remvoe headers

--- controllers/auth.php
+++ controllers/auth.php
@@ -6,13 +6,14 @@ require_once 'pdo.php';
 
 try {
     $pdo->beginTransaction();
-    $isValidSTMT = $pdo->prepare(file_get_contents(__DIR__ . '/../queries/backoffice/auth.sql'));
-    $isValidSTMT->execute([':id' => $_SESSION['id']]);
-    $isValid = $isValidSTMT->fetchColumn();
+    $hashPasswordSTMT = $pdo->prepare(file_get_contents(__DIR__ . '/../queries/backoffice/auth.sql'));
+    $hashPasswordSTMT->execute([':id' => $_SESSION['id']]);
+    $hashPassword = $hashPasswordSTMT->fetch(PDO::FETCH_ASSOC);
     error_log("auth");
-
-    if (!$_SESSION['session_id'] || password_verify($_SESSION['pass'], $isValid['mdp'])) {
-        header('Location: ../backoffice/connexion.php?error=3');
+    var_dump($hashPassword);
+    var_dump($_SESSION['session_id']);
+    if (!$_SESSION['session_id'] || !password_verify($_SESSION['pass'], $hashPassword['mdp'])) {
+        //header('Location: ../backoffice/connexion.php?error=3');
         die();
     }
 } catch (Exception $e) {
