cab5f4a33c0e8068c54ece339254795bfc9ba5b5
Zelirety <natuslanus@gmail.com>
2026-01-15 09:41:25 +0100

    Merge branch 'main' of https://github.com/Alizon-et-les-7-Nains/alizon

--- views/frontoffice/catalogue.php
+++ views/frontoffice/catalogue.php
@@ -237,7 +237,7 @@ $maxPrice = $maxPriceRow['maxPrix'] ?? 100;
                 <div class="nomEtPromo">
                     <h2 class="nomProduit"
                         onclick="window.location.href='produit.php?id=<?php echo $idProduit; ?>'">
-                        <?php if ($enRemise){echo "<h4 id='promoTexte'>Promo</h4>";} echo htmlspecialchars($value['nom']); ?></h2>
+                        <?php if ($enRemise){echo "<span id='promoTexte'>Promo</span>";} echo htmlspecialchars($value['nom']); ?></h2>
                 </div>
                 <div class="notation">
                     <?php if(number_format($value['note'], 1) == 0) { ?>
