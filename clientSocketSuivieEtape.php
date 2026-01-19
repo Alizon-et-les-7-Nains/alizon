782af2a390e98d0082395789a3cdb6fd64e5b4c3
pperche <perche.paolo@gmail.com>
2026-01-19 15:14:14 +0100

    debug $status reponse v3

--- clientSocketSuivieEtape.php
+++ clientSocketSuivieEtape.php
@@ -49,9 +49,8 @@ $bordereau = $result['noBordereau'];
 // Test 3: Consultation
 //echo "Test STATUS:\n";
 $status_response = send_command($socket, "STATUS $bordereau");
-$status_response = explode("|", $status_response);
-print_r($status_response);
-echo $bordereau;    
+//$status_response = explode("|", $status_response);
+echo($status_response);
 $sql = "UPDATE _commande SET etape = :etape WHERE idCommande = :idCommande";
 $stmt = $pdo->prepare($sql);
 $stmt->execute([":etape" => $status_response[4], ":idCommande" => $idCommande]);
