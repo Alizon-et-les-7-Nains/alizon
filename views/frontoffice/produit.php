a0858c26df5b07406cb3c88475de0428327c6496
Maaxxiimee <maximebourbon562@gmail.com>
2026-01-19 14:06:05 +0100

    fix: update signalement query to use product ID

--- views/frontoffice/produit.php
+++ views/frontoffice/produit.php
@@ -573,8 +573,8 @@ if ($produit['stock'] > 0) {
             <div>
                 
                 <?php
-                    $stmt = $pdo->prepare("SELECT DISTINCT titre FROM _signalement WHERE idClientSignale = 1;");
-                    $stmt->execute([$avis['idSignalement']]);
+                    $stmt = $pdo->prepare("SELECT DISTINCT titre FROM _signalement WHERE idProduitSignale = ?");
+                    $stmt->execute([$produit['idProduit']]);
                     $signalement = $stmt->fetch(PDO::FETCH_ASSOC);
                 ?>
                 
