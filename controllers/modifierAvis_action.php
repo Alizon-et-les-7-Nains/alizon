64c13d91dcfad8f9a0ae14fd5f56221a497d9567
Nosaphi <evan.legarrec56@gmail.com>
2026-01-20 10:11:51 +0100

    modif BDD

--- controllers/modifierAvis_action.php
+++ controllers/modifierAvis_action.php
@@ -102,7 +102,8 @@ if (!empty($_FILES['url']['name'])) {
 
     $extension = pathinfo($_FILES['url']['name'], PATHINFO_EXTENSION);
     $fileName = uniqid("avis_", true) . "." . $extension;
-    $filePath = $uploadDir . '/images/imagesAvis/' . $fileName; 
+    $filePath = $uploadDir . '/images/imagesAvis/' . $fileName;
+ 
     // Traitement de l'image
     try {
         treat($_FILES['url']['tmp_name'], $filePath);
