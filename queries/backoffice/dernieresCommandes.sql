d3b52fe7d110f7278bc24ec628da64eb4bea7c94
marechalg <guewenmarechal@protonmail.com>
2026-01-20 15:23:18 +0100

    Fixed query parameters method

--- queries/backoffice/dernieresCommandes.sql
+++ queries/backoffice/dernieresCommandes.sql
@@ -1,5 +1,5 @@
 select idProduit, dateCommande, etatLivraison, quantite, nom, prix, idVendeur, idCommande, idPanier
 from _contient natural join _produit natural join _commande
-where idVendeur = ?
+where idVendeur = :idVendeur
 order by dateCommande desc
 limit 6;
