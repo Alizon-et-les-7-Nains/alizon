d872811f91a46d697b759e4c5239fc9d8a7ca02e
pperche <perche.paolo@gmail.com>
2026-03-02 17:13:25 +0100

    update script to use default PHP path for execution

--- delivraptor/src/run_simulateur.sh
+++ delivraptor/src/run_simulateur.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/local/bin/php /var/www/html/delivraptor/src/simulateur.php >> /var/www/html/delivraptor/cron.log 2>&1
+php simulateur.php >> /var/www/html/delivraptor/cron.log 2>&1
