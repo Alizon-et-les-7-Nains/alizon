21e720584896e715d6a6c64a7fbda3fb7366e000
Nosaphi <evan.legarrec56@gmail.com>
2026-03-02 16:12:53 +0100

    bdd

--- views/frontoffice/catalogue.php
+++ views/frontoffice/catalogue.php
@@ -226,6 +226,8 @@ $cart = getCurrentCart($pdo, $idClient);
     <title>Catalogue</title>
     <link rel="icon" href="../../public/images/logoBackoffice.svg">
     <link rel="stylesheet" href="../../public/style.css">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
     <style>
     </style>
 </head>
