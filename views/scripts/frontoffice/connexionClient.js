d98141dee0ced7527fa04fc99df040417c4b9027
Timéo <timeodoom@gmail.com>
2026-03-03 11:13:46 +0100

    refactor: move QR code popup creation outside event listener for better readability

--- views/scripts/frontoffice/connexionClient.js
+++ views/scripts/frontoffice/connexionClient.js
@@ -2,8 +2,19 @@ import "https://cdn.jsdelivr.net/npm/qrcode/build/qrcode.min.js";
 
 const a2f = document.querySelector('.authenTwofacts input[type="checkbox"]');
 
+const qrCodePopup = document.createElement("div");
+qrCodePopup.classList.add("qr-code-popup");
+qrCodePopup.innerHTML = `
+            <div class="qr-code-content">
+                <h2>Scannez ce QR code avec votre application d'authentification</h2>
+                <img src="generate_qr_code.php" alt="QR Code">
+                <button id="closePopup">Fermer</button>
+            </div>
+        `;
+
 a2f.addEventListener("change", function () {
   if (this.checked) {
+    console.log("Activation de l'authentification à deux facteurs");
     // Envoyer une requête AJAX pour activer l'authentification à deux facteurs
     fetch("connexionClient.php", {
       method: "POST",
@@ -32,16 +43,6 @@ a2f.addEventListener("change", function () {
       });
 
     // Pop up avec qr code pour configurer l'authentification à deux facteurs
-
-    const qrCodePopup = document.createElement("div");
-    qrCodePopup.classList.add("qr-code-popup");
-    qrCodePopup.innerHTML = `
-            <div class="qr-code-content">
-                <h2>Scannez ce QR code avec votre application d'authentification</h2>
-                <img src="generate_qr_code.php" alt="QR Code">
-                <button id="closePopup">Fermer</button>
-            </div>
-        `;
     document.body.appendChild(qrCodePopup);
 
     // generation du QR code
