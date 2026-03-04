a3f833455e2531c9c561c34d61fac9dca33dd77f
pperche <perche.paolo@gmail.com>
2026-03-04 11:04:11 +0100

    refactor: add encryption and decryption functions for OTP secret handling

--- views/frontoffice/verify_otp.php
+++ views/frontoffice/verify_otp.php
@@ -7,12 +7,26 @@ use OTPHP\TOTP;
 
 header('Content-Type: application/json');
 
+function chiffrement($data) {
+    $key = 'la_super_cle_secrete';
+    $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('aes-256-cbc'));
+    $encrypted = openssl_encrypt($data, 'aes-256-cbc', $key, 0, $iv);
+    return base64_encode($iv . $encrypted);
+}
+
+function dechiffrement($data) {
+    $key = 'la_super_cle_secrete';
+    $data = base64_decode($data);
+    $iv_length = openssl_cipher_iv_length('aes-256-cbc');
+    $iv = substr($data, 0, $iv_length);
+    $encrypted = substr($data, $iv_length);
+    return openssl_decrypt($encrypted, 'aes-256-cbc', $key, 0, $iv);
+}
+
 // Récupérer les données JSON envoyées
 $data = json_decode(file_get_contents("php://input"), true);
 
 $otp = $data['otp'] ?? '';
-
-
 
 $user_id = $_SESSION['user_id'] ?? null;
 
@@ -25,6 +39,7 @@ if (!$user_id) {
 $stmt = $pdo->prepare("SELECT otp_secret FROM _client WHERE idClient = ?");
 $stmt->execute([$user_id]);
 $secret = $stmt->fetchColumn();
+$secret = dechiffrement($secret);
 
 var_dump($secret);
 
