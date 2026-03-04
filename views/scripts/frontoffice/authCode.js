60afdd63d3710d6ee4441d2dd1cb5db7dd19049c
pperche <perche.paolo@gmail.com>
2026-03-04 09:20:55 +0100

    log

--- views/scripts/frontoffice/authCode.js
+++ views/scripts/frontoffice/authCode.js
@@ -5,9 +5,4 @@ let num4 = document.getElementById("num4");
 let num5 = document.getElementById("num5");
 let num6 = document.getElementById("num6");
 
-console.log(num1);
-console.log(num2);
-console.log(num3);
-console.log(num4);
-console.log(num5);
-console.log(num6);
+console.log(num1.value);
