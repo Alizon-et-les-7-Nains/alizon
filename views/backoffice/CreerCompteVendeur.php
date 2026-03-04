43ee76a507f8b041cabb6bf84b63987f33ae84b7
Maaxxiimee <maximebourbon562@gmail.com>
2026-03-04 14:22:17 +0100

    map !

--- views/backoffice/CreerCompteVendeur.php
+++ views/backoffice/CreerCompteVendeur.php
@@ -379,6 +379,10 @@ unset($_SESSION['form_data']);
                 }
 
                 var map = L.map('map').setView([lat, lon], 13);
+                L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                    maxZoom: 19,
+                    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                }).addTo(map);
                 consolo.log(map);
 
         </script>
