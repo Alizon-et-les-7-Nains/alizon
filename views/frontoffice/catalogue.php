16b8e29d15397ceac899540de44ce41db40e74d3
Zelirety <natuslanus@gmail.com>
2026-03-04 14:31:49 +0100

    maxZoom: 8

--- views/frontoffice/catalogue.php
+++ views/frontoffice/catalogue.php
@@ -576,7 +576,7 @@ let adresses = <?= json_encode($adresses) ?>;
 var map = L.map('map').setView([48.174838642366915, -2.7538102129824145], 9);
 var group = L.markerClusterGroup();
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 10,
+    maxZoom: 8,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 map.addLayer(group);
