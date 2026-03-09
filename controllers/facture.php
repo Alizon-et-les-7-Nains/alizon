9965c3515bcce5cdd52693f049f662f964a1a65a
Nosaphi <evan.legarrec56@gmail.com>
2026-03-09 14:08:08 +0100

    modif requete 1

--- controllers/facture.php
+++ controllers/facture.php
@@ -19,7 +19,7 @@ $stmt = $pdo->prepare("
     FROM _commande c
     JOIN _panier p ON c.idPanier = p.idPanier
     JOIN _client cl ON p.idClient = cl.idClient 
-    NATUTRAL JOIN _adresseLivraison a 
+    JOIN _adresseLivraison a ON cl.idClient = a.idClient
     WHERE c.idCommande = :commande
     ");
 
