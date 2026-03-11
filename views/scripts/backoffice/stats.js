c1bce0c381ea634e16a3ddd5d89fc6fa7ab2b07b
marechalg <guewenmarechal@protonmail.com>
2026-03-11 11:14:02 +0100

    Debugging week stats

--- views/scripts/backoffice/stats.js
+++ views/scripts/backoffice/stats.js
@@ -128,6 +128,8 @@ function updateStats() {
                 argent.push(Object.values(weeksData)[month][w].argent);
             }
 
+            console.log(month);
+
             document.querySelector('article h3').innerHTML = getMonthLabel(month);
 
             chart = new Chart(canva, weekChart(vente, argent, Object.keys(weeksData[Object.keys(weeksData)[month]]) ?? ''));
