ac841163ff23068d34ff6f29a0f7f6fca103f4b5
pperche <perche.paolo@gmail.com>
2026-03-18 11:26:58 +0100

    debug: add print statement to output selected product IDs

--- controllers/genererCatalogue.php
+++ controllers/genererCatalogue.php
@@ -11,6 +11,8 @@
 
     $produits = explode(',', $_POST['selectedIds']);
 
+    print_r($produits);
+
     $placeholders = implode(',', array_fill(0, count($produits), '?'));
 
     $stmt = $pdo->prepare("
